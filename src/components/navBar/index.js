36d3734177ac53be824644ed1c6d19e7e25fffc6
Martin Azpiroz <martinazpiroz@MacBook-Pro-de-Martin.local>
2022-07-02 16:40:49 +0200

    Fonts - Title - Some tests

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './navBar.css'
+import Title from '../images/title.jpg'
 
 export const NavBar = () => {
 
 	return (
 			<div className="navBar">
 				<div>
-					<h1>Fernad Lems</h1>
+					<img src={Title} alt="Fernand Lems - Image - Title" />
 				</div>
-				<div>
-					<p>SHOP - ACCESORIES - HATS</p>
+				<div className="navList">
+					<p>SHOP - Accesories - HATS</p>
 				</div>
 		</div>
 
