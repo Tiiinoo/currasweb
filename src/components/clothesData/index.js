30f0c4faed506713929691cfdeb502445448b449
Martin Azpiroz <martinazpiroz@hotmail.com>
2022-07-07 00:54:11 +0200

    More items floating around

--- src/components/clothesData/index.js
+++ src/components/clothesData/index.js
@@ -1,11 +1,22 @@
-import Campera from '../images/campera.jpg'
+import Campera1 from '../images/campera1.jpg'
+import Campera2 from '../images/campera2.jpg'
 import Boina from '../images/boina.jpg'
-import Cartera from '../images/cartera.jpg'
-
+import Sombrero1 from '../images/sombrero1.jpg'
+import Sombrero2 from '../images/sombrero2.jpg'
+import Cartera1 from '../images/cartera1.jpg'
+import Cartera2 from '../images/cartera2.jpg'
+import Cartera3 from '../images/cartera3.jpg'
+import Cartera4 from '../images/cartera4.jpg'
 
 export const ClothesData = [
 	{
-		image: `${Campera}`,
+		image: `${Campera1}`,
+		alt: 'Campera Curra',
+		category: 'shop',
+
+	},
+	{
+		image: `${Campera2}`,
 		alt: 'Campera Curra',
 		category: 'shop',
 
@@ -17,15 +28,39 @@ export const ClothesData = [
 
 	},
 	{
-		image: `${Cartera}`,
+		image: `${Sombrero1}`,
+		alt: 'Sombrero Curra',
+		category: 'hats',
+
+	},
+	{
+		image: `${Sombrero2}`,
+		alt: 'Sombrero Curra',
+		category: 'hats',
+
+	},
+	{
+		image: `${Cartera1}`,
 		alt: 'Cartera Curra',
 		category: 'accesories',
 
 	},
 	{
-		image: 'https://www.freeiconspng.com/thumbs/blank-t-shirt-png/blank-t-shirt-png-28.png',
-		alt: 'Blue sleeveless t-shirt',
-		category: 'shop',
+		image: `${Cartera2}`,
+		alt: 'Cartera Curra',
+		category: 'accesories',
+
+	},
+	{
+		image: `${Cartera3}`,
+		alt: 'Cartera Curra',
+		category: 'accesories',
+
+	},
+	{
+		image: `${Cartera4}`,
+		alt: 'Cartera Curra',
+		category: 'accesories',
 
 	},
 ]
--- src/components/showClothes/index.js
+++ src/components/showClothes/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { ClothesData } from '../clothesData'
 import { MainThing } from '../mainThing'
+import { useParams } from 'react-router-dom'
 
 export function ShowClothes() {
 
+	
 	return (
 		<>
 			<MainThing slides={ ClothesData } />
