6df502ac27fa47f898256c124991a3c018a19adb
Martin Azpiroz <martinazpiroz@hotmail.com>
2023-01-17 01:48:07 +0100

    We're back

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,5 +1,36 @@
-import { Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import Cart from "../data/cart.png";
 
 export const NavBar = () => {
-  <h1>NavBar</h1>;
+  return (
+    <>
+      <NavbarBs>
+        <Container className="bg-white shadow-sm mb-3">
+          <Nav className="me-auto">
+            {/*this push something on the same level to the right (the shopping)*/}
+            <Nav.Link to="/" as={NavLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link to="/about" as={NavLink}>
+              About
+            </Nav.Link>
+            <Nav.Link to="/policies" as={NavLink}>
+              Policies
+            </Nav.Link>
+            <Nav.Link to="/contact" as={NavLink}>
+              Contact
+            </Nav.Link>
+          </Nav>
+          <Button
+            style={{ width: "3rem", height: "3rem", postion: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
+          >
+            <img scr={Cart}></img>
+          </Button>
+        </Container>
+      </NavbarBs>
+    </>
+  );
 };
